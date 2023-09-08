ffeddf1c950d0c51ca246c9e496d9836166319ef
Mathieu Colmon <mathieu@colmon.fr>
2023-09-08 21:53:54 +0200

    Try to fix gh action

--- tests/utils.ts
+++ tests/utils.ts
@@ -5,7 +5,7 @@ export const wait = (ms: number) => (
 export function calculateTimeGap(periods: { time: number }[]) {
   let minTimeGap = Infinity;
   for (let i = 1; i < 10; i += 1) {
-    const timeGap = periods[i - 1].time - periods[i].time;
+    const timeGap = periods[i - 1].time - periods[i]?.time;
     if (timeGap < minTimeGap) minTimeGap = timeGap;
   }
   return minTimeGap;
