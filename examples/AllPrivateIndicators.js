d2b4d589dc207bb5ea547d248d76517f110cda75
Daniel <d.groen@codefield.nl>
2024-02-29 16:31:01 +0100

    revert allprivateindicators

--- examples/AllPrivateIndicators.js
+++ examples/AllPrivateIndicators.js
@@ -1,11 +1,11 @@
-const TradingView = require("../main");
+const TradingView = require('../main');
 
 /**
  * This example creates a chart with all user's private indicators
  */
 
-if (!process.argv[2]) throw Error("Please specify your 'sessionid' cookie");
-if (!process.argv[3]) throw Error("Please specify your 'signature' cookie");
+if (!process.argv[2]) throw Error('Please specify your \'sessionid\' cookie');
+if (!process.argv[3]) throw Error('Please specify your \'signature\' cookie');
 
 const client = new TradingView.Client({
   token: process.argv[2],
@@ -13,22 +13,24 @@ const client = new TradingView.Client({
 });
 
 const chart = new client.Session.Chart();
-chart.setMarket("BINANCE:BTCEUR", {
-  timeframe: "D",
+chart.setMarket('BINANCE:BTCEUR', {
+  timeframe: 'D',
 });
 
 TradingView.getPrivateIndicators(process.argv[2]).then((indicList) => {
-  // console.log(indicList);
-  // indicList.forEach(async (indic) => {
-  //   const privateIndic = await indic.get();
-  //   console.log('Loading indicator', indic.name, '...');
-  //   const indicator = new chart.Study(privateIndic);
-  //   indicator.onReady(() => {
-  //     console.log('Indicator', indic.name, 'loaded !');
-  //   });
-  //   indicator.onUpdate(() => {
-  //     console.log('Plot values', indicator.periods);
-  //     console.log('Strategy report', indicator.strategyReport);
-  //   });
-  // });
+  indicList.forEach(async (indic) => {
+    const privateIndic = await indic.get();
+    console.log('Loading indicator', indic.name, '...');
+
+    const indicator = new chart.Study(privateIndic);
+
+    indicator.onReady(() => {
+      console.log('Indicator', indic.name, 'loaded !');
+    });
+
+    indicator.onUpdate(() => {
+      console.log('Plot values', indicator.periods);
+      console.log('Strategy report', indicator.strategyReport);
+    });
+  });
 });
