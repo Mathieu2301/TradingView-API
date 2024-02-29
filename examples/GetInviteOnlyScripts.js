4a6f8dfc964919f1879d9125a0678e29539a622c
Daniel <d.groen@codefield.nl>
2024-02-29 18:06:22 +0100

    Remove redundant code

--- examples/GetInviteOnlyScripts.js
+++ examples/GetInviteOnlyScripts.js
@@ -8,11 +8,6 @@ const TradingView = require("../main");
 if (!process.argv[2]) throw Error("Please specify your 'sessionid' cookie");
 if (!process.argv[3]) throw Error("Please specify your 'signature' cookie");
 
-const client = new TradingView.Client({
-  token: process.argv[2],
-  signature: process.argv[3],
-});
-
 TradingView.getInviteOnlyScripts(process.argv[2]).then((indicList) => {
   console.log(indicList);
 });
