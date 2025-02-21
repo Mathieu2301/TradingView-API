3404b8788739efb44a34cdcdcb71e35f527c47eb
Mathieu Colmon <mathieu@colmon.fr>
2025-02-21 02:44:42 +0100

    Fix authentication

--- src/miscRequests.js
+++ src/miscRequests.js
@@ -428,10 +428,11 @@ module.exports = {
    * @returns {Promise<User>} Token
    */
   async getUser(session, signature = '', location = 'https://www.tradingview.com/') {
-    const { data } = await axios.get(location, {
+    const { data, headers } = await axios.get(location, {
       headers: {
         cookie: genAuthCookies(session, signature),
       },
+      maxRedirects: 0,
       validateStatus,
     });
 
@@ -455,6 +456,10 @@ module.exports = {
         authToken: /"auth_token":"(.*?)"/.exec(data)?.[1],
         joinDate: new Date(/"date_joined":"(.*?)"/.exec(data)?.[1] || 0),
       };
+    }
+
+    if (headers.location !== location) {
+      return this.getUser(session, signature, headers.location);
     }
 
     throw new Error('Wrong or expired sessionid/signature');
