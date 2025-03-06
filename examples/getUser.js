41f0ebc603e0b26e21be1cd835eef40fc6356ba2
Daniel <d.groen@codefield.nl>
2025-03-06 20:59:46 +0100

    added more fields for getuser

--- examples/getUser.js
+++ examples/getUser.js
@@ -8,7 +8,7 @@ if (!process.argv[2]) throw Error('Please specify your session');
 if (!process.argv[3]) throw Error('Please specify your signature');
 
 TradingView.getUser(process.argv[2], process.argv[3]).then((data) => {
-  // console.log(data);
+  console.log(data);
 }).catch((err) => {
   console.error('Login error:', err.message);
 });
